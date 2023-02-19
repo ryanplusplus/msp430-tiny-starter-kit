e53d1460dd544415f86e0ae75e31f2e536f3896e
Ryan Hartlage <ryanplusplus@gmail.com>
2023-02-19 09:28:31 -0500

    LED might blink if there were a system tick

--- src/hardware/heartbeat.c
+++ src/hardware/heartbeat.c
@@ -3,6 +3,7 @@
  * @brief
  */
 
+#include <msp430.h>
 #include <stddef.h>
 #include "heartbeat.h"
 
@@ -14,10 +15,12 @@ static tiny_timer_t timer;
 
 static void blink(void* context)
 {
+  P1OUT ^= 0x01;
   (void)context;
 }
 
 void heartbeat_init(tiny_timer_group_t* timer_group)
 {
+  P1DIR |= 0x01;
   tiny_timer_start_periodic(timer_group, &timer, half_period_in_msec, NULL, blink);
 }
